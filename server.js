4151665f1999ba8d889b1d070ac635d6e9066282
Prashant <thapliyalprashant285@gmail.com>
2023-09-09 13:53:22 -0700

    remove mongoose

--- server.js
+++ server.js
@@ -11,20 +11,11 @@ const {
   userLeave,
   getRoomUsers,
 } = require("./utils/users");
-const { MongoClient } = require("mongodb");
 const moment = require("moment");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
-const mongoURI = process.env.MONGO_URI;
-
-async function connectToMongoDB() {}
-
-async function insertMessage(username, message, room, client) {}
-
-async function getMessages(room, client) {}
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -32,64 +23,58 @@ const botName = "ChitChat App";
 
 const chatMessages = [];
 
-(async () => {
-  const mongoClient = await connectToMongoDB();
-  io.on("connection", (socket) => {
-    socket.on("joinRoom", ({ username, room }) => {
-      const user = userJoin(socket.id, username, room);
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-      socket.join(user.room);
+    socket.join(user.room);
 
-      socket.emit("message", formatMessage(botName, "Welcome to ChitChat App"));
+    socket.emit("message", formatMessage(botName, "Welcome to ChitChat App"));
 
-      socket.broadcast
-        .to(user.room)
-        .emit(
-          "message",
-          formatMessage(botName, `${user.username} has joined the chat`)
-        );
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+
+    socket.emit("chatHistory", chatMessages);
+  });
+
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    const message = {
+      username: user.username,
+      text: msg,
+      time: moment().format("h:mm A"),
+    };
+    chatMessages.push(message);
+
+    io.to(user.room).emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
 
       io.to(user.room).emit("roomUsers", {
         room: user.room,
         users: getRoomUsers(user.room),
       });
-
-      socket.emit("chatHistory", chatMessages);
-    });
-
-    socket.on("chatMessage", async (msg) => {
-      const user = getCurrentUser(socket.id);
-
-      const message = {
-        username: user.username,
-        text: msg,
-        time: moment().format("h:mm A"),
-      };
-      chatMessages.push(message);
-
-      io.to(user.room).emit("message", message);
-
-      // If you want to save the message to MongoDB as well, you can do so here
-      // await insertMessage(user.username, msg, user.room, mongoClient);
-    });
-
-    socket.on("disconnect", () => {
-      const user = userLeave(socket.id);
-
-      if (user) {
-        io.to(user.room).emit(
-          "message",
-          formatMessage(botName, `${user.username} has left the chat`)
-        );
-
-        io.to(user.room).emit("roomUsers", {
-          room: user.room,
-          users: getRoomUsers(user.room),
-        });
-      }
-    });
+    }
   });
-})();
+});
 
 const PORT = process.env.PORT || 3000;
 
